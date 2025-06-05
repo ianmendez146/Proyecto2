3e024f02305ee2be0cf829eea99ddbff69e66b2d
EmeliePere <24000255@galileo.edu>
2025-06-04 19:18:07 -0600

    2048.py

--- 2048.py
+++ 2048.py
@@ -122,7 +122,107 @@ def limpiar_consola():
 def modo_1v1():
     print("Necesitas amigos para jugar este juego??? ")
 
+def modo_IA ():
+    def movimientos_ia (linea_ia):
+        direcciones_posibles = ["arriba", "abajo", "izquierda", "derecha"]
+        mejor_puntuacion_evaluada = -float('inf')
+        mejor_direccion = None
 
+        for direccion in direcciones_posibles:
+            tablero_simulado = deepcopy(tablero) 
+            
+            hubo_cambio_simulado, _ = mover_tablero(tablero_simulado, direccion) 
+            
+            if hubo_cambio_simulado:
+                puntuacion_actual_evaluada = evaluar_tablero(tablero_simulado)
+                
+                if puntuacion_actual_evaluada > mejor_puntuacion_evaluada:
+                    mejor_puntuacion_evaluada = puntuacion_actual_evaluada
+                    mejor_direccion = direccion
+
+        return mejor_direccion 
+
+    def analizar_tablero (tablero):
+        score = 0
+        
+        # Priorizar casillas vacías
+        vacios = 0
+        for r in range(4):
+            for c in range(4):
+                if tablero[r][c] == 0:
+                    vacios += 1
+        score += vacios * 20 
+
+        # Suma total de los valores del tablero
+        suma_total_valores = 0
+        for r in range(4):
+            for c in range(4):
+                suma_total_valores += tablero[r][c]
+        score += suma_total_valores * 0.5 
+
+        # Monotonicidad (orden) y Suavidad (diferencias pequeñas entre vecinos)
+        monotonicity_score = 0
+        smoothness_penalty = 0
+
+        dr = [0, 1] 
+        dc = [1, 0] 
+
+        for r in range(4):
+            for c in range(4):
+                if tablero[r][c] == 0:
+                    continue
+                current_val = tablero[r][c]
+                
+                for i in range(2): 
+                    nr, nc = r + dr[i], c + dc[i]
+                    
+                    if 0 <= nr < 4 and 0 <= nc < 4 and tablero[nr][nc] != 0:
+                        neighbor_val = tablero[nr][nc]
+                        smoothness_penalty -= abs(math.log2(current_val) - math.log2(neighbor_val)) * 2
+
+                        if current_val > neighbor_val:
+                            monotonicity_score += 1
+                        elif current_val < neighbor_val:
+                            monotonicity_score += 1
+        
+        score += monotonicity_score * 5 
+        score += smoothness_penalty      
+
+        # Priorizar el numero mas grande en una esquina 
+        max_val = 0
+        for r in range(4):
+            for c in range(4):
+                if tablero[r][c] > max_val:
+                    max_val = tablero[r][c]
+
+        corner_bonus = 0
+        if tablero[0][0] == max_val: 
+            corner_bonus = 500
+        elif tablero[0][3] == max_val or tablero[3][0] == max_val or tablero[3][3] == max_val:
+            corner_bonus = 200 
+        score += corner_bonus
+
+        # Contar posibles fusiones para el siguiente turno
+        potential_merges = 0
+        for r in range(4):
+            for c in range(3): 
+                if tablero[r][c] != 0 and tablero[r][c] == tablero[r][c+1]:
+                    potential_merges += 1
+            for c in range(4):
+                if r < 3 and tablero[r][c] != 0 and tablero[r][c] == tablero[r+1][c]:
+                    potential_merges += 1
+        
+        score += potential_merges * 15
+
+        return score
+ 
+def casillas_vacias (tablero):
+    contador_casillaV = 0
+    for fila in tablero:
+        for celda in tablero:
+            if celda == 0:
+                contador_casillaV += 1
+    return contador
  
 def casillas_vacias (tablero):
     contador_casillaV = 0
